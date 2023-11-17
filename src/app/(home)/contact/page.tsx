5dfe9eab8fdc71ffd03317053086ca314b7e4364
Piyush Yadav <piyushyadav0191@gmail.com>
2023-11-17 15:03:28 +0530

    completed: contact page

---
+++ src/app/(home)/contact/page.tsx
@@ -0,0 +1,25 @@
+import FAQ from '@/components/FAQ'
+import { Button, buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-32 mb-16'>
+      <h1 className='text-4xl text-center font-bold'>Frequently asked questions(FAQs)</h1>
+      <FAQ />
+      <div className='text-center mt-14'>
+        <h1 className='text-2xl font-semibold'>Still have questions?</h1>
+        <p className='text-xl text-zinc-500 mt-5'>Get in touch with our team.</p>
+        <Link href={"mailto:contact@codemate.ai"} className={buttonVariants({
+          size: "lg",
+          className: "text-xl mt-6"
+        })}>
+          Contact
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/FAQ.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { FAQ_CONTENT } from "@/constants/faq-content"
+
+type Props = {}
+
+const FAQ = (props: Props) => {
+  return (
+    <Accordion type="single" collapsible className="w-full mt-5">
+      {FAQ_CONTENT.map((item) => (
+        <>
+          <AccordionItem value={`item-${item.id}`}>
+            <AccordionTrigger className="text-2xl">{item.title} </AccordionTrigger>
+            <AccordionContent className="text-xl">
+              {item.answer}
+            </AccordionContent>
+          </AccordionItem>
+        </>
+      ))}
+
+    </Accordion>
+  )
+}
+
+export default FAQ
