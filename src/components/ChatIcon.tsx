b52f4e5466220c0aa3b58742f2c7e6cf072769f6
Piyush Yadav <piyushyadav0191@gmail.com>
2023-11-18 19:54:23 +0530

    Testing: Custom chatbot

--- src/components/ChatIcon.tsx
+++ src/components/ChatIcon.tsx
@@ -31,16 +31,16 @@ export default function ChatIcon() {
       </button>
       {isOpen && (
         <div className="fixed bottom-6 right-4 md:bottom-8 md:right-6 z-10">
-          <div className="bg-white rounded-lg w-80 h-96 flex flex-col shadow-lg">
+          {/* <div className="bg-white rounded-lg w-80 h-96 flex flex-col shadow-lg">
             <div className="p-4 text-center">
-              <h3 className="text-xl">Let's Chat</h3>
+              <h3 className="text-xl">Let&apos;s Chat</h3>
               <button onClick={handleClick} className="absolute right-0 top-0 mt-2 mr-2">
                 <XIcon size={30} />
               </button>
             </div>
             <hr />
             <div className="w-full p-3 overflow-auto flex-grow">
-              {/* Chat messages will go here */}
+            
               {messages.map((message, index) => (
                 <div key={index} className={`my-2 p-2 rounded-lg ${message.type === 'user' ? 'bg-blue-500 text-white ml-auto' : 'bg-gray-200 mr-auto'}`}>
                   {message.text}
@@ -51,7 +51,14 @@ export default function ChatIcon() {
               <input className="w-full rounded-full border px-4 py-2 mr-4" placeholder="Type a message" />
               <button className="bg-blue-500 text-white rounded-full px-4 py-2">Send</button>
             </div>
-          </div>
+          </div> */}
+          <iframe
+            src="https://www.chatbase.co/chatbot-iframe/3dibKToEdeVYVRZxRaLsL"
+            width="100%"
+
+            style={{ height: '100%', minHeight: '700px' }}
+          ></iframe>
+
         </div>
       )}
     </div>
